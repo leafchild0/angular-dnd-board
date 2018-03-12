30f88216759a5fbc16e4e108397837f906f163aa
Victor Malyshev <victor@V.local>
2018-03-12 11:12:15 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { ListComponent } from './components/list/list.component';
+import { TileComponent } from './components/tile/tile.component';
 
+import { ListService } from './services/list-service.service';
+import { UtilsService } from './services/utils.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ListService, UtilsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
